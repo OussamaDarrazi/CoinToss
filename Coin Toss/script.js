6bc498f34fbe97b06e92e084860e543c4f2951bf
Oussama Darrazi <34890717+nostradamus001@users.noreply.github.com>
2023-02-27 15:27:44 +0100

    Add files via upload

---
+++ Coin Toss/script.js
@@ -0,0 +1,10 @@
+var hotails = document.getElementById("head-or-tails");
+var coin = document.getElementById("coin");
+
+hotails.addEventListener("click", ()=>{
+    coin.classList.toggle('animating');
+})
+
+coin.addEventListener("animationend", ()=>{
+    coin.innerHTML = Math.random()>0.5 ? "Head" : "Tails";
+});
